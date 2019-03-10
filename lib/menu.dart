565ccc16bce922ec207b9f072958d747b959ac3d
Noah Cain <nmcain@gmail.com>
2019-03-10 13:38:35 -0700

    Update menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -11,26 +11,104 @@ class AppMenu extends StatelessWidget {
     return new Scaffold(
 
       body:
+
+
+
+
+
+      new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+
+
+
+
       new Container(
-        color: const Color(0xFFffffff),
+      color: const Color(0xFFffffff),
         padding: const EdgeInsets.all(0.0),
         alignment: Alignment.centerLeft,
         width: 1.7976931348623157e+308,
         height: 75.0,
-
-
         child: new SearchWidget(),
+      ),
 
 
 
 
+        new Container(
+          color: const Color(0xFFffffff),
+          padding: const EdgeInsets.all(0.0),
+          alignment: Alignment.centerLeft,
+          width: 250.0,
+          height: 75.0,
+          child: new WallpaperIcon(),
+        ),
+
+
+
+      new Container(
+        color: const Color(0xFFffffff),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.centerLeft,
+        width: 250.0,
+        height: 75.0,
+        child: new WallpaperIcon(),
+      ),
+
+      new Container(
+        color: const Color(0xFFffffff),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.centerLeft,
+        width: 250.0,
+        height: 75.0,
+        child: new WallpaperIcon(),
+      ),
+
+      new Container(
+        color: const Color(0xFFffffff),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.centerLeft,
+        width: 250.0,
+        height: 75.0,
+        child: new WallpaperIcon(),
+      ),
+
+      new Container(
+        color: const Color(0xFFffffff),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.centerLeft,
+        width: 250.0,
+        height: 75.0,
+        child: new WallpaperIcon(),
+      ),
+
+      new Container(
+        color: const Color(0xFFffffff),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.centerLeft,
+        width: 250.0,
+        height: 75.0,
+        child: new WallpaperIcon(),
+      ),
+
+
+
+
+
+          ]
 
       ),
 
 
 
 
-    );
+      );
+
+
+
+
 
 
 
