ba4477c753a6f29a7e65458cd471ab43c5be5472
Noah Cain <nmcain@gmail.com>
2019-03-10 13:11:52 -0700

    more updates

--- lib/apps.dart
+++ lib/apps.dart
@@ -1,5 +1,59 @@
+/*This will ultimately be replaced by a global system file, but keep it for android*/
+
+
+
 import 'dart:async';
 import 'package:flutter/material.dart';
+
+
+
+
+
+
+
+
+
+
+class AppGrid extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+
+
+    return new Container(
+      child:
+      new Image.asset(
+        'lib/images/wallpaper.png',
+        fit:BoxFit.fill,
+        width: 50.0,
+        height: 50.0,
+      ),
+
+      padding: const EdgeInsets.all(0.0),
+      alignment: Alignment.center,
+      width: 100.0,
+      height: 100.0,
+    );
+
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class WallpaperIcon extends StatelessWidget {
   @override
