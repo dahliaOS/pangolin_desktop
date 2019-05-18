fe7b78785bf945c2313e7947f8ef99e61660cf19
Camden Bruce <camden.o.b@gmail.com>
2019-05-18 12:25:36 +1200

    Merge Petal Text Editor With Pangolin

---
+++ lib/editor.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TextEditorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Petal Text Editor',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TextEditor(),
+    );
+  }
+}
+
+class TextEditor extends StatefulWidget {
+
+  final String title="Petal Text Editor";
+
+  @override
+  _TextEditorState createState() => _TextEditorState();
+}
+
+class _TextEditorState extends State<TextEditor> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        //padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: TextField(
+          style: Theme.of(context).textTheme.display1,
+          minLines: null,
+          maxLines: null,
+          expands: true,
+        ),
+      ),
+    );
+  }
+}
