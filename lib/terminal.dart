1637b0219647c72ef977019c07aca6f3e8582dac
Noah Cain <nmcain@gmail.com>
2019-07-18 18:59:39 -0400

    Add new terminal app base

---
+++ lib/terminal.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+   class CustomBarWidget extends StatelessWidget {
+
+      GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          key: _scaffoldKey,
+          body: Container(
+            height: 160.0,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  width: MediaQuery.of(context).size.width,
+                  height: 100.0,
+                  child: Center(
+                    child: Text(
+                      "Home",
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 80.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(1.0),
+                          border: Border.all(
+                              color: Colors.grey.withOpacity(0.5), width: 1.0),
+                          color: Colors.white),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              Icons.menu,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              print("your menu action here");
+                              _scaffoldKey.currentState.openDrawer();
+                            },
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Search",
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.search,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              print("your menu action here");
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.notifications,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              print("your menu action here");
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      }
+    }
+
+
+/*void main() {
+  runApp(new MyApp());
+}*/
+
+class Terminal extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.deepOrange,
+        primaryColor: const Color(0xFFff5722),
+        accentColor: const Color(0xFFff5722),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        body:
+          
+new CustomBarWidget(),
+
+
+          
+
+    
+      );
+    }
+}
+
+
