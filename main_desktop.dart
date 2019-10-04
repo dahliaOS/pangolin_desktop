8f46c10d028b58c871accaa8fb35e04f6d332ba3
Noah Cain <nmcain@gmail.com>
2019-10-03 21:06:36 -0700

    add updates

---
+++ main_desktop.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'mainapp.dart';
+
+void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  runApp(MainApp());
+}
+
