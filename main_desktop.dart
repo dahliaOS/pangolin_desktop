62af1447521e9ebe8eeb40bc3355f00a09353bc4
Noah Cain <nmcain@gmail.com>
2019-10-08 09:06:24 -0700

    fix dependencies

--- main_desktop.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
-import 'mainapp.dart';
-
-void main() {
-  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  runApp(MainApp());
-}
-
