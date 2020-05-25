f6a0b8e87fdb249b4db83e53d91a8f5950c2e691
Noah Cain <nmcain@gmail.com>
2020-05-25 16:17:22 -0700

    android hotfix!!!

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/nmcain/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nmcain/Desktop/pangolin-desktop"
+export "FLUTTER_APPLICATION_PATH=/Users/nmcain/Desktop/pangolin_desktop"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
