ac7d13eae4f5df44ced81152b340fa2611c6dd52
Noah Cain <nmcain@gmail.com>
2020-05-28 19:24:40 -0700

    Fix cards JSON error!
    You can now build a card in launcher.dart, using this method: buildCard(IconData icon, String title, Color color, Color splash, String text), example:  buildCard(Icons.info, 'System Information', Colors.blue, Colors.blue.withAlpha(30), 'You are on a pre-release development build!'),

--- lib/applications/cards.dart
+++ lib/applications/cards.dart
@@ -3,31 +3,8 @@ import 'package:flutter/material.dart';
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-class SysInfoCard extends StatelessWidget {
-  @override
-  Widget build (BuildContext context) {
-
       
-      
-      
-      
-    
-      
-      
-      
+ Card buildCard(IconData icon, String title, Color color, Color splash, String text) {
 return new Card(
   
             shape: RoundedRectangleBorder(
@@ -35,13 +12,14 @@ return new Card(
               ),
            
       child: InkWell(
-        splashColor: Colors.blue.withAlpha(30),
+        splashColor: splash,
         onTap: () {
           
           
-           showDialog(
-      context: context,
+  showDialog(
+
       builder: (BuildContext context) {
+        
         // return object of type Dialog
         return AlertDialog(
           title: new Text("Feature not implemented"),
@@ -58,7 +36,6 @@ return new Card(
         );
       },
     );
-        
           
           
         },
@@ -78,14 +55,14 @@ return new Card(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   new Icon(
-                    Icons.info,
-                    color: Colors.blue,
+                    icon,
+                    color: color,
                     size: 20.0),
     
                   new Text(
-                  " " + "System Information",
+                  " " + title,
                     style: new TextStyle(fontSize:15.0,
-                    color: Colors.blue,
+                    color: color,
                     fontWeight: FontWeight.w400,
                     fontFamily: "Roboto"),
                   )
@@ -97,7 +74,7 @@ return new Card(
               
               
               new Text(
-                  "pangolin-desktop, commit 'varCommit'",
+                 text,
                     style: new TextStyle(fontSize:15.0,
                     color: Colors.black,
                     fontWeight: FontWeight.w400,
@@ -114,129 +91,7 @@ return new Card(
         ),
       ),
     );
-      
-      
-      
-      
-      
-  }
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-class NewsCard extends StatelessWidget {
-  @override
-  Widget build (BuildContext context) {
-
-      
-      
-return new Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-           
-      child: InkWell(
-        splashColor: Colors.deepOrange.withAlpha(30),
-        onTap: () {
-         
-         
-         
-         
-         
-           showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Feature not implemented"),
-          content: new Text("This feature is currently not available on your build of Pangolin. Please see https://reddit.com/r/dahliaos to check for updates."),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-         
-         
-         
-         
-         
-         
-        },
-        child: Container(
-          width: 300,
-          height: 100,
-          child: new Padding(
-                padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
-               child:  new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  new Icon(
-                    Icons.speaker_notes,
-                    color: Colors.deepOrange,
-                    size: 20.0),
-    
-                  new Text(
-                  " " + "News",
-                    style: new TextStyle(fontSize:15.0,
-                    color: Colors.deepOrange,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Roboto"),
-                  )
-                ]
-    
-              ),
-              
-              
-              
-              
-              
-               new Text(
-                  "UNABLE TO PARSE JSON!!!",
-                    style: new TextStyle(fontSize:15.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Roboto"),
-                  )
-              
-              
-              
-            ]
-    
-          ),
- 
-              ),
-        ),
-      ),
-    );
-      
-      
-      
-      
-      
-  }
-}
