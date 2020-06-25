e7b0f42f8300f746b37c704e2c58221a6fec38dd
Noah Cain <nmcain@gmail.com>
2020-06-25 17:35:07 -0400

    add files.dart as a launchable application

--- lib/applications/files.dart
+++ lib/applications/files.dart
@@ -13,3 +13,41 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Files());
+}
+class Files extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      home: new FilesHome(),
+    );
+  }
+}
+
+class FilesHome extends StatefulWidget {
+  FilesHome({Key key}) : super(key: key);
+  @override
+  _FilesHomeState createState() => new _FilesHomeState();
+}
+
+class _FilesHomeState extends State<FilesHome> {
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Files'),
+          ),
+      );
+    }
+}
