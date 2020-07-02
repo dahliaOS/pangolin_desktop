9861ca7b5926e62a09f90c0613322fce452a82f9
Noah Cain <nmcain@gmail.com>
2020-07-02 13:14:47 -0400

    Wrap windows in cliprect to prevent contents from escaping graphically

--- lib/applications/containers.dart
+++ lib/applications/containers.dart
@@ -26,9 +26,15 @@ class Containers extends StatelessWidget {
       title: 'Generated App',
       theme: new ThemeData(
         primarySwatch: Colors.lightBlue,
-      
       ),
-      home: new ContainersApp(),
+
+       initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => FirstScreen(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => SecondScreen(),
+    },
     );
   }
 }
@@ -46,4 +52,44 @@ class _ContainersAppState extends State<ContainersApp> {
         body: new Container(),
       );
     }
+}
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Launch screen'),
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/second');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
 }
