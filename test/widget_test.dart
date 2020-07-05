8d5f7037e1d8e6778ba9e110be0efb0bdc5ff335
Blake Leonard <bleonard252@rocketmail.com>
2020-07-04 22:52:52 -0400

    Implement (failing) widget tests! `flutter test`

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,68 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:ui';
+
+import 'package:GeneratedApp/applications/terminal.dart';
+import 'package:GeneratedApp/launcher_toggle.dart';
+import 'package:GeneratedApp/status_tray.dart';
+import 'package:GeneratedApp/widgets/app_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:GeneratedApp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(MyApp());
+
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
+
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
+
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+  testWidgets('Try to open Files', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(
+      find.byWidgetPredicate(
+        (element) => (element is AppLauncherPanelButton && element.icon.endsWith("files.png"))
+      )
+    );
+    print((await tester.pumpAndSettle()).toString() + " frames on animation!");
+    // assert(tester.firstWidget<BackdropFilter>(
+    //   find.ancestor(
+    //     of: find.text("Files"), 
+    //     matching: find.byElementType(BackdropFilter)))
+    //   .filter != null,
+    //   "Files app bar should have a blur"
+    // ); //don't know why I did that one
+  });
+  testWidgets('Try to open Terminal', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.tap(
+      find.byWidgetPredicate(
+        (element) => (element is AppLauncherPanelButton && element.icon.endsWith("terminal.png"))
+      )
+    );
+    print((await tester.pumpAndSettle()).toString() + " frames on animation!");
+    //assert(tester.widget(find.byType(TerminalApp)).
+    //TODO(bleonard252): wait for Bash to be ready. if it errors, fail
+  });
+  testWidgets('Try to open Quick Settings', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byType(StatusTrayWidget));
+    print((await tester.pumpAndSettle()).toString() + " frames on animation!");
+  });
+  testWidgets('Try to open Launcher', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byType(LauncherToggleWidget));
+    print((await tester.pumpAndSettle()).toString() + " frames on animation!");
   });
 }
