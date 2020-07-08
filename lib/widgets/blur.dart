ed0fdcba700b8fd08d10d304027f558cc22037b3
larsb24 <larsb24@outlook.com>
2020-07-08 19:51:33 +0200

    Create Blur widget and make Blur toggle in settings more or less functional

---
+++ lib/widgets/blur.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+
+import 'package:GeneratedApp/settings/hiveManager.dart';
+import 'package:flutter/material.dart';
+
+class Blur extends StatefulWidget {
+  Widget child;
+  BorderRadius borderRadius;
+  Blur({Key key, @required this.child, borderRadius}) : super(key: key);
+
+  @override
+  _BlurState createState() => _BlurState();
+}
+
+class _BlurState extends State<Blur> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: widget.borderRadius == null
+          ? BorderRadius.circular(0)
+          : widget.borderRadius,
+      child: BackdropFilter(
+        filter: HiveManager().get("enableBlur")
+            ? ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0)
+            : ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0),
+        child: widget.child,
+      ),
+    );
+  }
+}
