17bf28a6c3576e33aa1109f0d8bad3be1ab9aa43
Noah Cain <nmcain@gmail.com>
2020-07-07 22:55:33 -0400

    Logs app

--- lib/applications/logging.dart
+++ lib/applications/logging.dart
@@ -8,7 +8,7 @@ class Logs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Generated App',
+      title: 'System Logs',
       theme: new ThemeData(
         primarySwatch: Colors.red,
       ),
@@ -24,11 +24,55 @@ class LogsPage extends StatefulWidget {
 }
 
 class _LogsPageState extends State<LogsPage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('App Name'),
+        title: new Text('System Logs'),
+      ),
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.selected,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.apps),
+                selectedIcon: Icon(Icons.apps),
+                label: Text('Application Logs'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.new_releases),
+                selectedIcon: Icon(Icons.new_releases),
+                label: Text('Event Logs'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                selectedIcon: Icon(Icons.settings),
+                label: Text('Service Logs'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.brightness_low),
+                selectedIcon: Icon(Icons.brightness_low),
+                label: Text('System Logs'),
+              ),
+            ],
+          ),
+          VerticalDivider(thickness: 1, width: 1),
+          // This is the main content.
+          Expanded(
+            child: Center(
+              child: Text('selectedIndex: $_selectedIndex'),
+            ),
+          )
+        ],
       ),
     );
   }
