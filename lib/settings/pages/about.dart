1563942653a9157f8ac6c3d66729829b252f411c
LarsB24 <larsb24@outlook.com>
2020-07-15 01:53:05 +0200

    Create About Page
    Signed-off-by: LarsB24 <larsb24@outlook.com>

--- lib/settings/pages/about.dart
+++ lib/settings/pages/about.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:Pangolin/widgets/settingsTile.dart';
 import 'package:flutter/material.dart';
 
 class About extends StatefulWidget {
@@ -21,7 +24,52 @@ class _AboutState extends State<About> {
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
                     fontFamily: "Roboto"),
-              ))
+              )),
+              SizedBox(height: 20),
+              Image(
+                image: AssetImage("lib/images/DahliaLogo.png"),
+                height: 200,
+              ),
+              SizedBox(height: 10),
+              Text(
+                "dahliaOS",
+                style: TextStyle(fontSize: 35),
+              ),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text("Version",
+                      style: TextStyle(
+                          fontSize: 17,
+                          letterSpacing: 0.2,
+                          fontWeight: FontWeight.bold)),
+                ),
+                SizedBox(height: 5),
+                SettingsTile(children: [Text("Version v200630.1")]),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text("Kernel",
+                      style: TextStyle(
+                          fontSize: 17,
+                          letterSpacing: 0.2,
+                          fontWeight: FontWeight.bold)),
+                ),
+                SizedBox(height: 5),
+                SettingsTile(children: [Text("Linux Kernel 5.6.15")]),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text("Pangolin Version",
+                      style: TextStyle(
+                          fontSize: 17,
+                          letterSpacing: 0.2,
+                          fontWeight: FontWeight.bold)),
+                ),
+                SizedBox(height: 5),
+                SettingsTile(children: [Text("Version v200713.2")]),
+                SizedBox(height: 20),
+              ]),
             ],
           ),
         ),
