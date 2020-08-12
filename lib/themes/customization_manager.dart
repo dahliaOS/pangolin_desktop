beb224e1088b03390997f5206d4442d4441b861c
LarsB24 <larsb24@outlook.com>
2020-08-12 14:47:24 +0200

    Fix Font in light theme
    Signed-off-by: LarsB24 <larsb24@outlook.com>

--- lib/themes/customization_manager.dart
+++ lib/themes/customization_manager.dart
@@ -5,36 +5,35 @@ import 'package:provider/provider.dart';
 class Themes {
   //light
   static ThemeData light(Color accent) {
-    return ThemeData.light().copyWith(
-      accentColor: accent,
-      toggleableActiveColor: accent,
-      primaryColor: accent,
-      iconTheme: IconThemeData(color: Colors.grey[700]),
-      textTheme: TextTheme(
-        headline5: TextStyle(color: Colors.black),
-        bodyText1: TextStyle(color: Colors.black),
-        bodyText2: TextStyle(color: Colors.black),
-        caption: TextStyle(color: Colors.black),
-      ),
-      sliderTheme: SliderThemeData(
-        activeTickMarkColor: accent,
-        thumbColor: accent,
-        activeTrackColor: accent,
-        inactiveTrackColor: accent.withAlpha(90),
-        inactiveTickMarkColor: accent,
-      ),
-      scaffoldBackgroundColor: Color.fromARGB(255, 247, 247, 247),
-      cardColor: Colors.grey[100],
-      inputDecorationTheme: InputDecorationTheme(
+    ThemeData(
+        accentColor: accent,
+        primaryColor: accent,
+        canvasColor: Color(0xff1B1B1D),
+        iconTheme: IconThemeData(color: Colors.white),
+        textTheme: TextTheme(
+          headline5: TextStyle(color: Colors.white),
+          bodyText1: TextStyle(color: Colors.white),
+          bodyText2: TextStyle(color: Colors.white),
+          caption: TextStyle(color: Colors.white),
+        ),
+        sliderTheme: SliderThemeData(
+          activeTickMarkColor: accent,
+          thumbColor: accent,
+          activeTrackColor: accent,
+          inactiveTrackColor: accent.withAlpha(90),
+          inactiveTickMarkColor: accent,
+        ),
+        toggleableActiveColor: accent,
+        cardColor: Colors.grey[900],
+        inputDecorationTheme: InputDecorationTheme(
           labelStyle: TextStyle(
             color: accent,
           ),
-          fillColor: Colors.grey[100]),
-      tabBarTheme: TabBarTheme(
-        labelColor: Colors.grey[200],
-      ),
-      textSelectionHandleColor: accent,
-    );
+        ),
+        tabBarTheme: TabBarTheme(labelColor: Colors.black12),
+        textSelectionHandleColor: accent,
+        brightness: Brightness.light,
+        platform: TargetPlatform.android);
   }
 
   //dark
