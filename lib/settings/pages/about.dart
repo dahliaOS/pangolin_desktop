8c0578d0bfb7d59b97cdcba70e38b78adb8a71d4
LarsB24 <larsb24@outlook.com>
2020-08-12 14:57:03 +0200

    Add warning to every settings page
    Signed-off-by: LarsB24 <larsb24@outlook.com>

--- lib/settings/pages/about.dart
+++ lib/settings/pages/about.dart
@@ -74,6 +74,43 @@ class _AboutState extends State<About> {
           ),
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+          color: Color(0x00ffffff),
+          child: new SizedBox(
+              height: 50,
+              width: 15,
+              child: new Padding(
+                  padding: EdgeInsets.all(0),
+                  child: Card(
+                    elevation: 0,
+                    color: Colors.amber[500],
+                    child: new SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: new Row(
+                        children: [
+                          new Center(
+                              child: new Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Icon(
+                                    Icons.warning,
+                                    size: 25,
+                                    color: Colors.grey[900],
+                                  ))),
+                          Center(
+                              child: new Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: new Text(
+                                    "WARNING: You are on a pre-release build of dahliaOS. Some settings don't work yet.",
+                                    style: new TextStyle(
+                                      color: Colors.grey[900],
+                                      fontSize: 14,
+                                      fontFamily: "Roboto",
+                                    ),
+                                  ))),
+                        ],
+                      ),
+                    ),
+                  )))),
     );
   }
 }
--- lib/settings/pages/customization.dart
+++ lib/settings/pages/customization.dart
@@ -293,6 +293,43 @@ class _CustomizationState extends State<Customization> {
           ),
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+          color: Color(0x00ffffff),
+          child: new SizedBox(
+              height: 50,
+              width: 15,
+              child: new Padding(
+                  padding: EdgeInsets.all(0),
+                  child: Card(
+                    elevation: 0,
+                    color: Colors.amber[500],
+                    child: new SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: new Row(
+                        children: [
+                          new Center(
+                              child: new Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Icon(
+                                    Icons.warning,
+                                    size: 25,
+                                    color: Colors.grey[900],
+                                  ))),
+                          Center(
+                              child: new Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: new Text(
+                                    "WARNING: You are on a pre-release build of dahliaOS. Some settings don't work yet.",
+                                    style: new TextStyle(
+                                      color: Colors.grey[900],
+                                      fontSize: 14,
+                                      fontFamily: "Roboto",
+                                    ),
+                                  ))),
+                        ],
+                      ),
+                    ),
+                  )))),
     );
   }
 
