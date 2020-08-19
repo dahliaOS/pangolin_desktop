5cdbdecc9f0a5769e88cb33b906ab8f23b727c4a
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2020-08-19 12:51:48 -0400

    Update developer.dart
    fix poweroff problem

--- lib/applications/developer.dart
+++ lib/applications/developer.dart
@@ -64,7 +64,7 @@ class _DeveloperAppPageState extends State<DeveloperAppPage> {
                   child: Text('Reboot')),
               RaisedButton(
                   onPressed: () {
-                    Process.run('shutdown', ['-h', 'now']);
+                    Process.run('poweroff', [' ']);
                   },
                   child: Text('Shutdown')),
               RaisedButton(
