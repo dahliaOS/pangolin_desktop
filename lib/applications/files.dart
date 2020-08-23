c41fc1bf1c127ad6e2cb977644b0d419f18a206b
LarsB24 <larsb24@outlook.com>
2020-08-23 16:25:25 +0200

    Files: reduce elevation

--- lib/applications/files.dart
+++ lib/applications/files.dart
@@ -273,6 +273,7 @@ class _FilesHomeState extends State<FilesHome> {
             color: Colors.white,
             width: 250,
             child: Drawer(
+              elevation: 3.0,
               // Add a ListView to the drawer. This ensures the user can scroll
               // through the options in the drawer if there isn't enough vertical
               // space to fit everything.
