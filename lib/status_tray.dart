4b4c5b6f512a475a7d81083188bfcf3ae1817dca
Noah Cain <nmcain@gmail.com>
2020-09-18 13:08:05 -0700

    Decrease size of clock to small

--- lib/status_tray.dart
+++ lib/status_tray.dart
@@ -109,7 +109,7 @@ class StatusTrayWidgetState extends State<StatusTrayWidget> {
                     ),
                     Text(
                       _timeString,
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+                      style: TextStyle(fontSize: 15, color: Colors.white),
                     ),
                   ]),
             ),
