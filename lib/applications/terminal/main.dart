1a218a9c8abf78aa2e503923bb903ec5ae11ea8c
Noah Cain <nmcain@gmail.com>
2020-09-26 21:56:10 -0700

    Show emulated PS1 output in terminal

--- lib/applications/terminal/main.dart
+++ lib/applications/terminal/main.dart
@@ -13,6 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'terminal-widget.dart';
 
@@ -29,6 +32,7 @@ class TerminalApp extends StatelessWidget {
         primaryColor: const Color(0xFF212121),
         accentColor: const Color(0xFFff6507),
         canvasColor: const Color(0xFF303030),
+        platform: TargetPlatform.fuchsia,
       ),
       initialRoute: '/',
       routes: {
@@ -187,21 +191,66 @@ class TerminalUIState extends State<TerminalUI> with TickerProviderStateMixin {
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF282828),
-        title: Text("Settings"),
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xFF282828),
+            title: Text("Settings"),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "Appearance",
+                ),
+                Tab(text: "Keyboard & mouse"),
+                Tab(
+                  text: "Behavior",
+                ),
+                Tab(
+                  text: "About",
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              AppearanceWidget(),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ));
+  }
+}
+
+Widget themeCard(Color bgcolor, Color fgcolor) {
+  return new Container(
+    color: bgcolor,
+    child: new Text(
+      "user@dahliaOS~\$",
+      style: new TextStyle(color: fgcolor, fontFamily: "Cousine"),
+    ),
+  );
+}
+
+class AppearanceWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Container(
+      padding: EdgeInsets.all(25),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          new Text(
+            "Theme",
+            style: new TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          )
+        ],
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
-    );
+    ));
   }
 }
