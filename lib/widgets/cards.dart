c69efc7909e80f3b9f7f8ca05253f19470ad274f
SincerelyFaust <hedesmarin@gmail.com>
2020-09-27 14:05:34 +0200

    Fixed some paths

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -131,7 +131,8 @@ Expanded tileSection(BuildContext context) {
             AppLauncherButton(
                 type: AppLauncherButtonType.Drawer,
                 app: Welcome(),
-                icon: 'assets/images/dahlia.png',
+                icon:
+                    'assets/images/dahliaOS/Logos/compiled/dahliaOS_logo_drop_shadow.png',
                 label: local.get("app_welcome"),
                 color: Colors.grey[900],
                 callback: toggleCallback),
