ddc59ab50e88e43e2c9fa26366779aca998f1487
larsb24 <larsb24@outlook.com>
2020-11-02 17:24:30 +0100

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
-import 'package:flutter/services.dart';
 
 import 'package:Pangolin/desktop/desktop.dart';
 import 'package:Pangolin/utils/localization/localization.dart';
