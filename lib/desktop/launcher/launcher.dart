aab4cb37a970385fbc86d7c406bc3591e97b2a18
larsb24 <larsb24@outlook.com>
2020-12-09 21:25:43 +0100

    Launcher: close when clicking in empty space

--- lib/desktop/launcher/launcher.dart
+++ lib/desktop/launcher/launcher.dart
@@ -15,6 +15,8 @@ limitations under the License.
 */
 
 import 'package:Pangolin/internal/locales/locale_strings.g.dart';
+import 'package:Pangolin/utils/others/functions.dart';
+import 'package:Pangolin/utils/others/key_ring.dart';
 import 'package:Pangolin/utils/widgets/blur.dart';
 import 'package:Pangolin/desktop/launcher/applications.dart';
 import 'package:Pangolin/utils/widgets/cards.dart';
@@ -67,73 +69,79 @@ class LauncherState extends State<LauncherWidget> {
         accentColor: const Color(0xFFff5722),
         canvasColor: Colors.black.withOpacity(0.1),
       ),
-      home: new Stack(
-        children: [
-          new Blur(
-            child: new Container(
-              decoration:
-                  new BoxDecoration(color: Colors.black.withOpacity(0.5)),
-            ),
-          ),
-          new Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new SearchWidget(LocaleStrings.pangolin.launcherSearch),
-                  const SizedBox(height: 35),
-                  new SingleChildScrollView(
-                      padding: new EdgeInsets.only(
-                          left: 10.0, right: 10.0, top: 5.0),
-                      scrollDirection: Axis.horizontal,
-                      child: new Row(children: <Widget>[
-                        buildCard(
-                          Icons.brightness_low,
-                          LocaleStrings.pangolin.launcherCardSystemTitle,
-                          Colors.deepOrange,
-                          Colors.deepOrange.withAlpha(30),
-                          LocaleStrings.pangolin.launcherCardSystemValue,
-                          context,
-                        ),
-                        buildCard(
-                          Icons.info,
-                          LocaleStrings.pangolin.launcherCardInformationTitle,
-                          Colors.blue,
-                          Colors.blue.withAlpha(30),
-                          LocaleStrings.pangolin.launcherCardInformationValue,
-                          context,
-                        ),
-                        buildCard(
-                          Icons.music_note,
-                          LocaleStrings.pangolin.launcherCardMusicTitle,
-                          Colors.lightGreen,
-                          Colors.lightGreen.withAlpha(30),
-                          LocaleStrings.pangolin.launcherCardMusicValue,
-                          context,
-                        ),
-                        buildCard(
-                          Icons.lock,
-                          LocaleStrings.pangolin.launcherCardSecurityTitle,
-                          Colors.red,
-                          Colors.red.withAlpha(30),
-                          LocaleStrings.pangolin.launcherCardSecurityValue,
-                          context,
-                        ),
-                        buildCard(
-                          Icons.memory,
-                          LocaleStrings.pangolin.launcherCardKernelTitle,
-                          Colors.pink,
-                          Colors.pink.withAlpha(30),
-                          LocaleStrings.pangolin.launcherCardKernelValue,
-                          context,
-                        ),
-                      ])),
-                  tileSection(context),
-                ],
+      home: GestureDetector(
+        onTap: () {
+          setOverlayVisibility(
+              overlay: KeyRing.launcherOverlayKey, visible: false);
+        },
+        child: new Stack(
+          children: [
+            new Blur(
+              child: new Container(
+                decoration:
+                    new BoxDecoration(color: Colors.black.withOpacity(0.5)),
               ),
             ),
-          ),
-        ],
+            new Scaffold(
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new SearchWidget(LocaleStrings.pangolin.launcherSearch),
+                    const SizedBox(height: 35),
+                    new SingleChildScrollView(
+                        padding: new EdgeInsets.only(
+                            left: 10.0, right: 10.0, top: 5.0),
+                        scrollDirection: Axis.horizontal,
+                        child: new Row(children: <Widget>[
+                          buildCard(
+                            Icons.brightness_low,
+                            LocaleStrings.pangolin.launcherCardSystemTitle,
+                            Colors.deepOrange,
+                            Colors.deepOrange.withAlpha(30),
+                            LocaleStrings.pangolin.launcherCardSystemValue,
+                            context,
+                          ),
+                          buildCard(
+                            Icons.info,
+                            LocaleStrings.pangolin.launcherCardInformationTitle,
+                            Colors.blue,
+                            Colors.blue.withAlpha(30),
+                            LocaleStrings.pangolin.launcherCardInformationValue,
+                            context,
+                          ),
+                          buildCard(
+                            Icons.music_note,
+                            LocaleStrings.pangolin.launcherCardMusicTitle,
+                            Colors.lightGreen,
+                            Colors.lightGreen.withAlpha(30),
+                            LocaleStrings.pangolin.launcherCardMusicValue,
+                            context,
+                          ),
+                          buildCard(
+                            Icons.lock,
+                            LocaleStrings.pangolin.launcherCardSecurityTitle,
+                            Colors.red,
+                            Colors.red.withAlpha(30),
+                            LocaleStrings.pangolin.launcherCardSecurityValue,
+                            context,
+                          ),
+                          buildCard(
+                            Icons.memory,
+                            LocaleStrings.pangolin.launcherCardKernelTitle,
+                            Colors.pink,
+                            Colors.pink.withAlpha(30),
+                            LocaleStrings.pangolin.launcherCardKernelValue,
+                            context,
+                          ),
+                        ])),
+                    tileSection(context),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
