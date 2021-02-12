a1490ac44e180e59abc4ed97c52253ec24f835bf
Lars <larsb24@outlook.com>
2021-02-12 11:05:09 +0100

    [Action Center] create overlay

--- lib/desktop/taskbar_elements/action_center.dart
+++ lib/desktop/taskbar_elements/action_center.dart
@@ -1,7 +1,7 @@
 import 'package:dahlia_backend/dahlia_backend.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:pangolin/settings/settings.dart';
+import 'package:pangolin/widgets/qs_button.dart';
 import 'package:pangolin/utils/globals.dart';
 import 'package:pangolin/utils/pangolin_custom_icons_icons.dart';
 import 'package:pangolin/utils/wm_api.dart';
@@ -44,54 +44,122 @@ class ActionCenter extends StatelessWidget {
     final _wm = context.watch<WindowHierarchyState>();
     return Positioned(
       bottom: _wm.insets.bottom + 20,
-      left: sidePadding(context, 540),
-      right: sidePadding(context, 540),
+      left: sidePadding(context, 500),
+      right: sidePadding(context, 500),
       child: BoxContainer(
         useSystemOpacity: true,
         color: Theme.of(context).cardColor,
         customBorderRadius: BorderRadius.circular(10),
-        width: 540,
+        width: 500,
         height: 320,
         //margin: EdgeInsets.only(bottom: wmKey.currentState!.insets.bottom + 20),
-        child: SizedBox(
-          height: 48,
-          child: Column(
-            children: [
-              BoxContainer(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                color: Theme.of(context).cardColor,
-                useSystemOpacity: true,
-                height: 48,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      child: Icon(Icons.power_settings_new_rounded),
-                      mouseCursor: SystemMouseCursors.click,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 48,
+              child: Column(
+                children: [
+                  BoxContainer(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    color: Theme.of(context).cardColor,
+                    useSystemOpacity: true,
+                    height: 48,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          child: Icon(Icons.power_settings_new_rounded),
+                          mouseCursor: SystemMouseCursors.click,
+                        ),
+                        Text(
+                          "Action Center",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        InkWell(
+                          child: Icon(Icons.settings_outlined),
+                          mouseCursor: SystemMouseCursors.click,
+                          onTap: () {
+                            WmAPI(context).popOverlayEntry(
+                                Provider.of<DismissibleOverlayEntry>(context,
+                                    listen: false));
+                            WmAPI(context).openApp("io.dahlia.settings");
+                            /* WmAPI(context).pushWindowEntry(
+                                WindowEntry.withDefaultToolbar(
+                                    content: Settings(),
+                                    initialSize: Size(1280, 720))); */
+                          },
+                        ),
+                      ],
                     ),
-                    Text(
-                      "Action Center",
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    InkWell(
-                      child: Icon(Icons.settings_outlined),
-                      mouseCursor: SystemMouseCursors.click,
-                      onTap: () {
-                        WmAPI(context).popOverlayEntry(
-                            Provider.of<DismissibleOverlayEntry>(context,
-                                listen: false));
-                        WmAPI(context).openApp("io.dahlia.settings");
-                        /* WmAPI(context).pushWindowEntry(
-                            WindowEntry.withDefaultToolbar(
-                                content: Settings(),
-                                initialSize: Size(1280, 720))); */
-                      },
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      QuickSettingsButton(
+                          title: "Power\nMode",
+                          icon: Icons.battery_charging_full_outlined),
+                      QuickSettingsButton(
+                        title: "Blue light\nfilter",
+                        icon: Icons.brightness_4,
+                      ),
+                      QuickSettingsButton(
+                          title: "Ambient\nDisplay",
+                          icon: Icons.access_time_outlined),
+                      QuickSettingsButton(
+                        title: "Screen\nRecorder",
+                        icon: Icons.fiber_manual_record_outlined,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        child: Icon(Icons.volume_down),
+                      ),
+                      SizedBox(
+                        width: 360,
+                        child: Slider(
+                          onChanged: (details) {},
+                          value: 0.5,
+                        ),
+                      ),
+                      InkWell(
+                        child: Icon(Icons.arrow_forward_ios),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        child: Icon(Icons.brightness_5),
+                      ),
+                      SizedBox(
+                        width: 360,
+                        child: Slider(
+                          onChanged: (details) {},
+                          value: 0.5,
+                        ),
+                      ),
+                      InkWell(
+                        child: Icon(Icons.arrow_forward_ios),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
