6d501b891a8e2b44ad55e1464a95ee3bbd719a60
Lars <larsb24@outlook.com>
2021-02-13 03:08:12 +0100

    Create Search Overlay

--- lib/desktop/taskbar_elements/search.dart
+++ lib/desktop/taskbar_elements/search.dart
@@ -1,5 +1,11 @@
+import 'package:dahlia_backend/dahlia_backend.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:pangolin/utils/globals.dart';
+import 'package:pangolin/utils/wm_api.dart';
+import 'package:pangolin/widgets/searchbar.dart';
+import 'package:utopia_wm/wm.dart';
+import 'package:provider/provider.dart';
 
 class SearchButton extends StatelessWidget {
   @override
@@ -13,7 +19,10 @@ class SearchButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(4),
           hoverColor: Theme.of(context).cardColor.withOpacity(0.5),
           mouseCursor: SystemMouseCursors.click,
-          onTap: () {},
+          onTap: () {
+            WmAPI(context).pushOverlayEntry(
+                DismissibleOverlayEntry(uniqueId: "search", content: Search()));
+          },
           child: Padding(
             padding: EdgeInsets.all(12),
             child: Center(child: Icon(Icons.search, size: 20)),
@@ -21,5 +30,64 @@ class SearchButton extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      final _ac = context.watch<DismissibleOverlayEntry>().animation;
+      return AnimatedBuilder(
+          animation: _ac,
+          builder: (context, child) {
+            return Positioned(
+              top: 64,
+              left: sidePadding(context, 600),
+              right: sidePadding(context, 600),
+              child: BoxContainer(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                      offset:
+                          Offset(2.0, 2.0), // shadow direction: bottom right
+                    )
+                  ],
+                ),
+                useSystemOpacity: true,
+                color: Theme.of(context).scaffoldBackgroundColor,
+                customBorderRadius: BorderRadius.circular(10),
+                width: 500,
+                height: 320 * _ac.value,
+                //margin: EdgeInsets.only(bottom: wmKey.currentState!.insets.bottom + 20),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 52,
+                      child: Column(
+                        children: [
+                          BoxContainer(
+                              //padding: EdgeInsets.symmetric(horizontal: 16),
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              useSystemOpacity: true,
+                              height: 52,
+                              child: Searchbar(
+                                controller: TextEditingController(),
+                                hint: '"Search Device, Apps and Web',
+                                leading: Icon(Icons.search),
+                                trailing: Icon(Icons.more_vert_rounded),
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          });
+    });
   }
 }
