b04ecf5e6a98744d7e14b3f820314c7649c36110
Lars <larsb24@outlook.com>
2021-02-16 00:41:24 +0100

    Add more apps to the Launcher

--- lib/utils/app_list.dart
+++ lib/utils/app_list.dart
@@ -14,10 +14,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import 'package:Media/main.dart';
 import 'package:calculator/calculator.dart';
 import 'package:dahlia_backend/dahlia_backend.dart';
+import 'package:dahlia_clock/main.dart';
+import 'package:files/main.dart';
+import 'package:graft/main.dart';
 import 'package:pangolin/settings/settings.dart';
 import 'package:pangolin/widgets/error_window.dart';
+import 'package:system_logs/main.dart';
+import 'package:task_manager/monitor.dart';
+import 'package:terminal/terminal-widget.dart';
+import 'package:text_editor/editor.dart';
+import 'package:web_browser/main.dart';
+import 'package:welcome/main.dart';
 
 Map<String, Application> applications = {
   "io.dahlia.settings": Application(
@@ -31,6 +41,66 @@ Map<String, Application> applications = {
       name: "Calculator",
       iconName: "calculator",
       category: ApplicationCategory.OFFICE,
+      description: ""),
+  "io.dahlia.terminal": Application(
+      app: Terminal(),
+      name: "Terminal",
+      iconName: "terminal",
+      category: ApplicationCategory.SYSTEM,
+      description: ""),
+  "io.dahlia.editor": Application(
+      app: TextEditorApp(),
+      name: "Text Editor",
+      iconName: "notes",
+      category: ApplicationCategory.OFFICE,
+      description: ""),
+  "io.dahlia.graft": Application(
+      app: Graft(),
+      name: "Graft",
+      iconName: "graft",
+      category: ApplicationCategory.SYSTEM,
+      description: ""),
+  "io.dahlia.web": Application(
+      app: Browser(),
+      name: "Web Browser",
+      iconName: "web",
+      category: ApplicationCategory.INTERNET,
+      description: ""),
+  "io.dahlia.files": Application(
+      app: Files(),
+      name: "Files",
+      iconName: "files",
+      category: ApplicationCategory.SYSTEM,
+      description: ""),
+  "io.dahlia.media": Application(
+      app: Media(),
+      name: "Media",
+      iconName: "photos",
+      category: ApplicationCategory.MEDIA,
+      description: ""),
+  "io.dahlia.clock": Application(
+      app: Clock(),
+      name: "Clock",
+      iconName: "clock",
+      category: ApplicationCategory.SYSTEM,
+      description: ""),
+  "io.dahlia.logs": Application(
+      app: Logs(),
+      name: "System Logs",
+      iconName: "logs",
+      category: ApplicationCategory.SYSTEM,
+      description: ""),
+  "io.dahlia.welcome": Application(
+      app: Welcome(),
+      name: "Welcome",
+      iconName: "welcome-info",
+      category: ApplicationCategory.SYSTEM,
+      description: ""),
+  "io.dahlia.taskmanager": Application(
+      app: Tasks(),
+      name: "Task Manager",
+      iconName: "task",
+      category: ApplicationCategory.SYSTEM,
       description: ""),
 };
 
