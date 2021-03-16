687a57f770a7b4b53253bdea77837643625604e8
Noah Cain <nmcain@gmail.com>
2021-03-15 19:11:18 -0700

    Automatic dart changes

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,14 +2,15 @@
 // Generated file. Do not edit.
 //
 
-// ignore_for_file: lines_longer_than_80_chars
+// ignore: unused_import
+import 'dart:ui';
 
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(Registrar registrar) {
-  SharedPreferencesPlugin.registerWith(registrar);
-  registrar.registerMessageHandler();
+void registerPlugins(PluginRegistry registry) {
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
+  registry.registerMessageHandler();
 }
