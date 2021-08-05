05d7de2c02850678a53df7aad33bcb60fce998a8
Marin Heđeš <hedesmarin@gmail.com>
2021-08-05 20:53:17 +0200

    Friendly generated Windows files

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <battery_plus_windows/battery_plus_windows_plugin.h>
+#include <url_launcher_windows/url_launcher_plugin.h>
+#include <windows_path_provider/windows_path_provider_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   BatteryPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("BatteryPlusWindowsPlugin"));
+  UrlLauncherPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+  WindowsPathProviderPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowsPathProviderPlugin"));
 }
