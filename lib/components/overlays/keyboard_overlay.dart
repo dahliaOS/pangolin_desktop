b2db75c23d6e90ce82055442f012c052c040f92b
Lars <larsb24@outlook.com>
2021-10-18 21:39:52 +0200

    remove unused imports

--- lib/components/overlays/keyboard_overlay.dart
+++ lib/components/overlays/keyboard_overlay.dart
@@ -14,13 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import 'dart:async';
-
-import 'package:dahlia_backend/dahlia_backend.dart';
 import 'package:flutter/material.dart';
-import 'package:pangolin/utils/data/common_data.dart';
-import 'package:pangolin/utils/data/globals.dart';
-import 'package:pangolin/components/shell/shell.dart';
 
 void ShowKeyboard(context) {
   List<String> row1 = [
