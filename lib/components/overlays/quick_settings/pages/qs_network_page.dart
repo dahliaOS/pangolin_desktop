fa8a85f38d6cfd4bcbaf6851394d72b548bd869c
Lars <larsb24@outlook.com>
2021-12-04 00:55:06 +0100

    Fix placement of const

--- lib/components/overlays/quick_settings/pages/qs_network_page.dart
+++ lib/components/overlays/quick_settings/pages/qs_network_page.dart
@@ -7,8 +7,8 @@ class QsNetworkPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: QsTitlebar(
+    return Scaffold(
+      appBar: const QsTitlebar(
         title: 'Network Settings',
       ),
       body: Center(
