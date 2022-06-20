f62990681c41d2acce853600fce2057e3bbabf25
Noah Cain <nmcain@gmail.com>
2022-06-19 21:32:13 -0400

    Add new welcome screen

--- lib/components/desktop/welcome_screen.dart
+++ lib/components/desktop/welcome_screen.dart
@@ -15,7 +15,121 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => Navigator.pop(context),
-      child: BoxSurface(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          color: Colors.transparent,
+          child: Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                  /*boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          spreadRadius: 10,
+                          blurRadius: 7,
+                          offset: Offset(0, 0),
+                        ),
+                      ],*/
+                ),
+                width: 640,
+                height: 480,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: Image.asset(
+                        'assets/images/other/developer-white.png',
+                        fit: BoxFit.cover,
+                        filterQuality: FilterQuality.high,
+                        height: 256,
+                        width: 256,
+                      ),
+                    ),
+                    Positioned(
+                      top: 40,
+                      left: 40,
+                      child: Image.asset(
+                        'assets/images/logos/dahliaOS-modern.png',
+                        filterQuality: FilterQuality.high,
+                        fit: BoxFit.cover,
+                        height: 32,
+                      ),
+                    ),
+                    Positioned(
+                      top: 132,
+                      left: 40,
+                      width: 368,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Welcome!",
+                            style: TextStyle(
+                              fontSize: 48,
+                              fontWeight: FontWeight.w100,
+                              fontFamily: "Roboto",
+                            ),
+                          ),
+                          Text(
+                            "\nFor now, dahliaOS is pre-release software. Some features are incomplete, applications may not work as expected, and the experience may not be stable on certain devices. \n\nWe are always looking to improve our software, so feel free to share feedback with us on any of our social media. Have fun!",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Roboto",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 30,
+                      left: 30,
+                  
+                      child:
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Checkbox(
+                                value: showAtStartup,
+                                onChanged: (val) {
+                                  setState(() {
+                                    showAtStartup = !showAtStartup;
+                                  });
+                                },
+                              ),
+                              const Text("Show at every startup"),
+                            ],
+                          ),
+                         
+                       
+                    ),
+                    Positioned(bottom: 30,
+                    right: 30,
+                      child:  ElevatedButton(
+                            onPressed: () {
+                              DatabaseManager.set(
+                                  "initialStart", showAtStartup);
+
+                              Navigator.pop(context);
+                            },
+                            child: Padding(
+                              padding: ThemeConstants.buttonPadding,
+                              child: const Text("LET'S GO!"),
+                            ),
+                          ),)
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      /*BoxSurface(
         borderRadius: context.commonData.borderRadiusBig,
         margin: EdgeInsets.symmetric(
           horizontal: MediaQuery.of(context).size.width / 3.75,
@@ -74,7 +188,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             ],
           ),
         ),
-      ),
+      ),*/
     );
   }
 }
