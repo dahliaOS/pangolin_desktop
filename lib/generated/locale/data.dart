f772a5aa1d1d96727248a26aa090ceec06510cdd
SincerelyFaust <hedesmarin@gmail.com>
2022-12-04 17:30:49 +0100

    fix(locales): run locally

--- lib/generated/locale/data.dart
+++ lib/generated/locale/data.dart
@@ -1,45 +1,47 @@
 import 'package:yatl_gen/yatl_gen.dart';
-part './data/pl-PL.g.dart';
-part './data/hi-IN.g.dart';
-part './data/ro-RO.g.dart';
-part './data/fr-FR.g.dart';
-part './data/he-IL.g.dart';
-part './data/hu-HU.g.dart';
-part './data/ar-SA.g.dart';
-part './data/cs-CZ.g.dart';
-part './data/it-IT.g.dart';
 part './data/tr-TR.g.dart';
-part './data/pt-PT.g.dart';
-part './data/uk-UA.g.dart';
-part './data/nl-NL.g.dart';
-part './data/pt-BR.g.dart';
-part './data/es-ES.g.dart';
-part './data/hr-HR.g.dart';
+part './data/fr-FR.g.dart';
 part './data/de-DE.g.dart';
+part './data/pl-PL.g.dart';
+part './data/es-ES.g.dart';
+part './data/ar-SA.g.dart';
+part './data/hu-HU.g.dart';
+part './data/nl-NL.g.dart';
+part './data/ro-RO.g.dart';
+part './data/hi-IN.g.dart';
+part './data/he-IL.g.dart';
+part './data/cs-CZ.g.dart';
+part './data/hr-HR.g.dart';
+part './data/pt-PT.g.dart';
+part './data/pt-BR.g.dart';
+part './data/it-IT.g.dart';
+part './data/sv-SE.g.dart';
 part './data/en-US.g.dart';
+part './data/uk-UA.g.dart';
 
 class GeneratedLocales extends Locales {
   const GeneratedLocales()
       : super(
           locales: const [
-            _$PlPLLocaleData._(),
-            _$HiINLocaleData._(),
-            _$RoROLocaleData._(),
-            _$FrFRLocaleData._(),
-            _$HeILLocaleData._(),
-            _$HuHULocaleData._(),
-            _$ArSALocaleData._(),
-            _$CsCZLocaleData._(),
-            _$ItITLocaleData._(),
             _$TrTRLocaleData._(),
-            _$PtPTLocaleData._(),
-            _$UkUALocaleData._(),
-            _$NlNLLocaleData._(),
-            _$PtBRLocaleData._(),
-            _$EsESLocaleData._(),
-            _$HrHRLocaleData._(),
+            _$FrFRLocaleData._(),
             _$DeDELocaleData._(),
+            _$PlPLLocaleData._(),
+            _$EsESLocaleData._(),
+            _$ArSALocaleData._(),
+            _$HuHULocaleData._(),
+            _$NlNLLocaleData._(),
+            _$RoROLocaleData._(),
+            _$HiINLocaleData._(),
+            _$HeILLocaleData._(),
+            _$CsCZLocaleData._(),
+            _$HrHRLocaleData._(),
+            _$PtPTLocaleData._(),
+            _$PtBRLocaleData._(),
+            _$ItITLocaleData._(),
+            _$SvSELocaleData._(),
             _$EnUSLocaleData._(),
+            _$UkUALocaleData._(),
           ],
         );
 }
