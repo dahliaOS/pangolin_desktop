041b87a3e96731285789aed0aa8a808409d0ad7e
Camden Bruce <camden.o.b@gmail.com>
2023-01-14 22:57:48 +1300

    Fixes for zenit again

--- lib/components/overlays/quick_settings/widgets/qs_slider.dart
+++ lib/components/overlays/quick_settings/widgets/qs_slider.dart
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import 'package:flutter/material.dart';
 import 'package:pangolin/utils/extensions/extensions.dart';
 import 'package:zenit_ui/zenit_ui.dart';
 
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,17 @@
 #include "generated_plugin_registrant.h"
 
 #include <battery_plus_windows/battery_plus_windows_plugin.h>
+#include <screen_retriever/screen_retriever_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
+#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   BatteryPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("BatteryPlusWindowsPlugin"));
+  ScreenRetrieverPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
