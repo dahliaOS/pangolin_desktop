7f9b827286ab3e2bb54d7c0c4e27087469894113
Lars <larsb24@outlook.com>
2023-06-10 00:13:32 +0200

    Bring back _BuiltInApplicationService

--- lib/services/application.dart
+++ lib/services/application.dart
@@ -3,8 +3,13 @@ import 'dart:io';
 
 import 'package:collection/collection.dart';
 import 'package:dahlia_shared/dahlia_shared.dart';
+import 'package:flutter/material.dart';
+import 'package:pangolin/components/window/window_surface.dart';
+import 'package:pangolin/components/window/window_toolbar.dart';
 import 'package:pangolin/services/langpacks.dart';
+import 'package:pangolin/services/wm.dart';
 import 'package:pangolin/utils/extensions/extensions.dart';
+import 'package:pangolin/utils/wm/wm.dart';
 import 'package:path/path.dart' as p;
 import 'package:xdg_desktop/xdg_desktop.dart';
 import 'package:xdg_directories/xdg_directories.dart' as xdg;
@@ -18,7 +23,7 @@ abstract class ApplicationService extends ListenableService<ApplicationService> 
 
   static ApplicationService build() => _LinuxApplicationService();
 
-  factory ApplicationService.fallback() = _DummyApplicationService;
+  factory ApplicationService.fallback() = _BuiltInApplicationService;
 
   List<DesktopEntry> listApplications();
 
@@ -145,23 +150,84 @@ class _LinuxApplicationService extends ApplicationService {
   }
 }
 
-class _DummyApplicationService extends ApplicationService {
+class _BuiltInApplicationService extends ApplicationService {
+  final List<_BuiltinDesktopEntry> entries = [];
+  final Map<DesktopEntry, Widget> builders = {};
+
+  static WindowEntry windowEntry = const WindowEntry(
+    features: [
+      ResizeWindowFeature(),
+      SurfaceWindowFeature(),
+      FocusableWindowFeature(),
+      ToolbarWindowFeature(),
+    ],
+    layoutInfo: FreeformLayoutInfo(
+      position: Offset(32, 32),
+      size: Size(1280, 720),
+    ),
+    properties: {
+      ResizeWindowFeature.minSize: Size(480, 360),
+      SurfaceWindowFeature.elevation: 4.0,
+      SurfaceWindowFeature.shape: Constants.mediumShape,
+      SurfaceWindowFeature.background: PangolinWindowSurface(),
+      ToolbarWindowFeature.widget: PangolinWindowToolbar(
+        barColor: Colors.transparent,
+        textColor: Colors.black,
+      ),
+      ToolbarWindowFeature.size: 40.0,
+    },
+  );
+
   @override
-  DesktopEntry? getApp(String name) {
-    return listApplications().firstWhere((element) => element.name.main == name);
+  void start() {}
+
+  @override
+  _BuiltinDesktopEntry? getApp(String name) => entries.firstWhereOrNull((e) => e.id == name);
+
+  @override
+  List<_BuiltinDesktopEntry> listApplications() => entries;
+
+  @override
+  void startApp(String name) {
+    final _BuiltinDesktopEntry? app = getApp(name);
+
+    if (app == null) {
+      logger.warning(
+        "The specified app $name can't be opened as it doesn't exist",
+      );
+      return;
+    }
+
+    WindowManagerService.current.push(
+      windowEntry.newInstance(
+        content: app.content,
+        overrideProperties: {
+          WindowExtras.stableId: app.id,
+          WindowEntry.title: app.name.main,
+          WindowEntry.icon: app.icon != null
+              ? AssetImage(
+                  app.icon!.main.toResource().resolve().toString(),
+                )
+              : null,
+        },
+      ),
+    );
   }
 
   @override
-  List<DesktopEntry> listApplications() {
-    return [];
+  void stop() {
+    entries.clear();
+    builders.clear();
   }
+}
 
-  @override
-  FutureOr<void> start() {}
+class _BuiltinDesktopEntry extends DesktopEntry {
+  final Widget content;
 
-  @override
-  FutureOr<void> startApp(String name) {}
-
-  @override
-  FutureOr<void> stop() {}
+  const _BuiltinDesktopEntry({
+    required super.id,
+    required this.content,
+    required super.type,
+    required super.name,
+  });
 }
